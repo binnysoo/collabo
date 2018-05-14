5fdde10320e4acf6704ad1537d32f099fd6aabd2
jiwonl <abcljw93@gmail.com>
2018-05-14 11:51:14 +0000

    Post Model 기능추가

--- db/migrate/20180514111409_create_posts.rb
+++ db/migrate/20180514111409_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      
+      t.string :title
+      t.string :contents
 
       t.timestamps null: false
     end
