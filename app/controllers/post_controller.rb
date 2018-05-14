fbad053ed122ff1f2da61ef303bb05bea4adf43c
Soobin Kim <bin430@naver.com>
2018-05-14 11:42:40 +0000

    Post Controller 생성!

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,20 @@
 class PostController < ApplicationController
+    
+    def main
+        @post = Post.all
+    end
+    
+    def new
+    end
+    
+    def create
+        @post = Post.new
+        @post.title = params[:title]
+        @post.content = params[:content]
+        
+        @post.save
+        
+        redirect_to '/post/main'
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  
+  root 'post#main'
+  
+  get 'post/main' => 'home#main'
+  get 'post/new' => 'post#new'
+  get 'post/create' => 'post#create'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
